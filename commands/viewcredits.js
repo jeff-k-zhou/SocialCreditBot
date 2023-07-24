d02bc437dac50e28a3a0e115b5e91ed8fdf9f6fc
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-07-24 17:00:46 -0500

    hotfix 2

--- commands/viewcredits.js
+++ commands/viewcredits.js
@@ -16,7 +16,7 @@ module.exports = {
         ),
     async execute(interaction) {
         const target = interaction.options.getUser("target")
-        if (!(target.bot)) {
+        if (!(target.client.user.bot)) {
             await getDoc(doc(db, "servers", interaction.guild.id)).then((docSnap) => {
                 if (docSnap.exists()) {
                     let embed;
