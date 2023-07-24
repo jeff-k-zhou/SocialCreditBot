23c169842c3090d02e0ecc276031bc7c2d8567a3
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-07-24 16:50:15 -0500

    bot hotfix

--- commands/viewcredits.js
+++ commands/viewcredits.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder } = require("discord.js")
 const { ViewCredits } = require("../embeds")
 const { getDoc, doc } = require("firebase/firestore")
 const db = require("../firebase")
+require("dotenv").config()
+const botid = process.env.BOTID
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,18 +16,22 @@ module.exports = {
         ),
     async execute(interaction) {
         const target = interaction.options.getUser("target")
-        await getDoc(doc(db, "servers", interaction.guild.id)).then((docSnap) => {
-            if (docSnap.exists()) {
-                let embed;
-                if (!target) {
-                    embed = ViewCredits(interaction.user.username, docSnap.data()[interaction.user.id])
+        if (!(target.id === botid)) {
+            await getDoc(doc(db, "servers", interaction.guild.id)).then((docSnap) => {
+                if (docSnap.exists()) {
+                    let embed;
+                    if (!target) {
+                        embed = ViewCredits(interaction.user.username, docSnap.data()[interaction.user.id])
+                    } else {
+                        embed = ViewCredits(target.username, docSnap.data()[target.id])
+                    }
+                    interaction.reply({ embeds: [embed] })
                 } else {
-                    embed = ViewCredits(target.username, docSnap.data()[target.id])
+                    interaction.reply({ content: "Error: User does not exist. DM longhua for support."})
                 }
-                interaction.reply({ embeds: [embed] })
-            } else {
-                interaction.reply("Error: User does not exist. DM longhua for support.")
-            }
-        })
-    } 
+            })
+        } else {
+            interaction.reply({ content: "I have no credits.", ephemeral: true })
+        }
+    }
 }
