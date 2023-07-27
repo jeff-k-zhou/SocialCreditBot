f285275648b2debdebabb4287d19800125e04449
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-07-26 19:26:26 -0500

    checklog

---
+++ commands/checklog.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js")
+const { doc, getDoc } = require("firebase/firestore")
+const db = require("../firebase")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("checklog")
+        .setDescription("returns current log channel")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        interaction.deferReply({ epheremal: true }).then(() => {
+            getDoc(doc(db, interaction.guild.id, "info")).then((docSnap) => {
+                if (docSnap.data().log) {
+                    interaction.editReply({ content: `Log channel: <#${docSnap.data().log}>` })
+                } else {
+                    interaction.editReply({ content: "No log channel set. Use /setlog to set." })
+                }
+            })
+        })
+    }
+}
